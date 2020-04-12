8f2a9512da7249ba5c6f2ab1a6a981f242589ee4
Umesh Kumar05 <Umesh_Kumar05@ad.infosys.com>
2020-04-12 22:15:47 +0530

    Updates

---
+++ precache-manifest.2e5cf5032ecacdf5c1bb702c209a3e36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb94ade753dd7276d1a92b4872fa27c1",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "90f9ce7b29e5dbecc12b",
+    "url": "/covid19/static/css/main.0dcb7d46.chunk.css"
+  },
+  {
+    "revision": "a8d5f599418ae1754db9",
+    "url": "/covid19/static/js/2.bdceaf34.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.bdceaf34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f9ce7b29e5dbecc12b",
+    "url": "/covid19/static/js/main.3672009c.chunk.js"
+  },
+  {
+    "revision": "384fdf37c74f6a26a4c0",
+    "url": "/covid19/static/js/runtime-main.2993b3e3.js"
+  },
+  {
+    "revision": "9722e66405bc35def7338bac883b85f2",
+    "url": "/covid19/static/media/image.9722e664.png"
+  }
+]);
